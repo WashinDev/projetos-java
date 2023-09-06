2b44777185f3a44e08ce6a337eb69d679dade954
washnn <nascimentowashington789@gmail.com>
2023-09-05 21:52:54 -0300

    postmapping

--- api/src/main/java/br/com/projetoapi/api/controller/Controle.java
+++ api/src/main/java/br/com/projetoapi/api/controller/Controle.java
@@ -1,8 +1,13 @@
 package br.com.projetoapi.api.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.projetoapi.api.model.Pessoa;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 public class Controle {
@@ -20,6 +25,11 @@ public class Controle {
     @GetMapping("/boasVindas/{nome}")
     public String boasVinda(@PathVariable String nome) {
         return "Seja Bem Vindo(a) " + nome;
+    }
+
+    @PostMapping("/pessoa")
+    public Pessoa pessoa(@RequestBody Pessoa p) {
+        return p;
     }
 
 }
