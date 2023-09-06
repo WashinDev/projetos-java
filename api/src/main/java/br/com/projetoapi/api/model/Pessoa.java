7191c57663531c29184db0db40cbbbb23a2d9701
washnn <nascimentowashington789@gmail.com>
2023-09-06 09:09:02 -0300

    Entity and GeneratedValue mysql

--- api/src/main/java/br/com/projetoapi/api/model/Pessoa.java
+++ api/src/main/java/br/com/projetoapi/api/model/Pessoa.java
@@ -1,7 +1,18 @@
 package br.com.projetoapi.api.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "pessoas")
 public class Pessoa {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int codigo;
     private String nome;
     private int idade;
 
@@ -21,4 +32,11 @@ public class Pessoa {
         this.idade = idade;
     }
 
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
 }
