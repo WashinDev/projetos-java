4c4af29aa26f0800c80caa969324be644170fa30
washnn <nascimentowashington789@gmail.com>
2023-09-06 18:19:05 -0300

    servico etc

--- api/src/main/java/br/com/projetoapi/api/controller/Controle.java
+++ api/src/main/java/br/com/projetoapi/api/controller/Controle.java
@@ -7,6 +7,10 @@ import br.com.projetoapi.api.repo.Repo;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -39,6 +43,52 @@ public class Controle {
         return acao.save(obj);
     }
 
+    @DeleteMapping("/api/{codigo}")
+    public void remover(@PathVariable int codigo) {
+        Pessoa obj = selecionarPeloCodigo(codigo);
+        acao.delete(obj);
+    }
+
+    @GetMapping("/api/contador")
+    public long contador() {
+        return acao.count();
+    }
+
+    @GetMapping("/api/ordenarNomes")
+    public List<Pessoa> ordenarNomes() {
+        return acao.findByOrderByNome();
+    }
+
+    @GetMapping("/api/ordenarNomes2")
+    public List<Pessoa> ordenarNomes2() {
+        return acao.findByNomeOrderByIdade("wash");
+    }
+
+    @GetMapping("/api/nomeContem")
+    public List<Pessoa> nomeContem() {
+        return acao.findByNomeContaining("w");
+    }
+
+    @GetMapping("/api/iniciaCom")
+    public List<Pessoa> iniciaCom() {
+        return acao.findByNomeStartsWith("w");
+    }
+
+    @GetMapping("/api/terminaCom")
+    public List<Pessoa> terminaCom() {
+        return acao.findByNomeEndsWith("w");
+    }
+
+    @GetMapping("/api/somaIdades")
+    public int somaIdades() {
+        return acao.somaIdades();
+    }
+
+    @GetMapping("/api/idadeMaiorIugal")
+    public List<Pessoa> idadeMaiorIgual() {
+        return acao.idadeMaiorIgual(18);
+    }
+
     @GetMapping("")
     public String mensagem() {
         return "Hello World";
@@ -57,6 +107,11 @@ public class Controle {
     @PostMapping("/pessoa")
     public Pessoa pessoa(@RequestBody Pessoa p) {
         return p;
+    }
+
+    @GetMapping("/status")
+    public ResponseEntity<?> status() {
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
 }
---
+++ api/src/main/java/br/com/projetoapi/api/servico/Servico.java
@@ -0,0 +1,8 @@
+package br.com.projetoapi.api.servico;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Servico {
+    
+}
