ba664f4e06a0230209e69742befedd4d369de487
Washington <nascimentowashington789@gmail.com>
2023-10-06 08:52:13 -0300

    Getter and setters

---
+++ spring-boot-todo-app/src/test/java/com/wash/springboottodoapp/controllers/TodoItemController.java
@@ -0,0 +1,32 @@
+package com.wash.springboottodoapp.controllers;
+
+import java.time.Instant;
+import java.time.ZoneId;
+
+import jakarta.validation.Valid;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class TodoItemController {
+    private final Logger logger = LoggerFactory.getLogger(TodoItemController.class);
+
+    @GetMapping("/")
+    public ModelAndView index() {
+        logger.info("request to GET index");
+        ModelAndView modelAndView = new ModelAndView("index");
+        modelAndView.addObject("todoItems", todoItemRepository.findAll());
+        modelAndView.addObject("today", Instant.now().atZone(ZoneId.systemDefault()).toLocalDate().getDayOfWeek());
+        return modelAndView;
+    }
+}
